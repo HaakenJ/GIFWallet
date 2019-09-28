8fe57d6dd0a3204a3b067c3545fcd65c7167c810
Haakasaurus <kramerhjohnson@gmail.com>
2019-09-28 14:00:26 -0700

    Added todo items

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,14 @@
 /* TODO: Add functionality to the 'add a button' button.
     - Figure out how to remember users and create a user's gif page.
+        - Will need a save button next to each gif
     - Figure out how to select multiple buttons to combine search terms
     - Add drop down for number of results and rating
-    - List the gif's rating at the top of each gif */
+    - List the gif's rating at the top of each gif, use it like a navbar with button groups
+    - Make your list of buttons actually a drop down menu
+    - Add a jumbotron with info about the page.
+    - Add a theme of colors and text to the page.
+    - use .trim() with an user input to be defensive against a-holes.
+    - Add progress or spinners using anime.js for when gifs are loading.*/
 
 
 $(document).ready(() => {
@@ -20,8 +26,8 @@ $(document).ready(() => {
     function playAndPause() {
         var currentState = $(this).attr('data-state');
         if (currentState === 'still') {
-            $(this).attr('src', $(this).attr('data-playing'));
-            $(this).attr('data-state', 'playing');
+            $(this).attr('src', $(this).attr('data-animated'));
+            $(this).attr('data-state', 'animated');
         } else {
             $(this).attr('src', $(this).attr('data-still'));
             $(this).attr('data-state', 'still');
@@ -46,10 +52,10 @@ $(document).ready(() => {
         newButton('sport', item);
     })
 
-    $('.query-button').on('click', function () {
+    $(document.body).on('click', '.query-button', function () {
         queryTerm = $(this).text();
         queryUrl = `http://api.giphy.com/v1/gifs/search?q=${queryTerm}&rating=${rating}&limit=${numOfResults}&api_key=${apiKey}`;
-        $('.gifs').empty();
+        $('#gifs').empty();
         $.ajax({
             url: queryUrl,
             method: 'GET'
@@ -62,9 +68,9 @@ $(document).ready(() => {
                     'alt': item.title,
                     'data-state': 'still',
                     'data-still': item.images.fixed_height_still.url,
-                    'data-playing': item.images.fixed_height.url
+                    'data-animated': item.images.fixed_height.url
                 }).addClass('m-2 new-gif');
-                $('.gifs').append(newGif);
+                $('#gifs').append(newGif);
             })
         })
     })
@@ -72,10 +78,7 @@ $(document).ready(() => {
     $('.container').on('click', '.new-gif', playAndPause);
 
     $('#submit').on('click', function() {
-        console.log('clicked');
-        var newTerm = $('#new-button').val(),
-            newCat = $('#new-category').val();
-        console.log(newTerm, newCat);
-        newButton(newCat, newTerm);
+        var newTerm = $('#new-button').val();
+        newButton('personal', newTerm);
     })
 })
