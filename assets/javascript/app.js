9fb530e80e9f64c9a58676179dafa28f19000fc2
Haakasaurus <kramerhjohnson@gmail.com>
2019-09-30 12:34:14 -0700

    Completed storage of personal buttons, added trending button, added random button, added dropdown menus for changing number of results and rating, added save buttons so a user can save gifs for later use.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,10 @@
 
 $(document).ready(() => {
 
+
+
+    // VARIABLES
+
     var animal = ['dog', 'cat', 'rabbit', 'hamster', 'badger', 'dolphin', 'whale', 'platypus', 'kangaroo', 'wolverine', 'pig', 'horse', 'cow', 'snake'],
         emotion = ['happy', 'funny', 'sad', 'angry', 'offended', 'bored', 'awkward', 'confused', 'excited', 'frustrated', 'hungry', 'tired', 'disappointed', 'embarrassed', 'scared', 'shocked', 'surprised', 'nervous'],
         sport = ['baseball', 'football', 'basketball', 'hockey', 'soccer', 'cricket', 'rugby', 'rock climbing', 'snowboarding', 'skiing', 'skateboarding', 'wakeboarding', 'e-sports'],
@@ -25,14 +29,27 @@ $(document).ready(() => {
         rating = 'g',
         queryTerm = 'dog',
         queryUrl = `http://api.giphy.com/v1/gifs/search?q=${queryTerm}&rating=${rating}&limit=${numOfResults}&api_key=${apiKey}`,
-        personalButtons;
+        personalButtons,
+        savedGifs;
 
 
-    if (localStorage.getItem(personalButtons) === null) {
+    if (localStorage.getItem('personalButtons') === null) {
         personalButtons = [];
     } else {
-        personalButtons = localStorage.getItem(personalButtons).split(';');
+        personalButtons = localStorage.getItem('personalButtons').split(',');
+        console.log(personalButtons);
     }
+
+    if (localStorage.getItem('savedGifs') === null) {
+        savedGifs = [];
+    } else {
+        savedGifs = JSON.parse(localStorage.getItem('savedGifs'));
+        console.log(savedGifs);
+    }
+
+
+
+    // FUNCTIONS
 
     function playAndPause() {
         var currentState = $(this).attr('data-state');
@@ -53,6 +70,36 @@ $(document).ready(() => {
         $(newCat).append(newBtn);
     }
 
+    function showSignUp() {
+        $('#login-collapse').hide();
+        $('#login-collapse').attr('data-toggle', 'hidden');
+        $('#sign-up-collapse').show();
+        signUpDisplayAnime.play();
+        $('#sign-up-collapse').attr('data-toggle', 'displayed');
+    }
+
+    function showLogin() {
+        $('#sign-up-collapse').hide();
+        $('#sign-up-collapse').attr('data-toggle', 'hidden');
+        $('#login-collapse').show();
+        loginDisplayAnime.play();
+        $('#login-collapse').attr('data-toggle', 'displayed');
+    }
+
+    function setNumOfResults() {
+        numOfResults = $(this).attr('value');
+        console.log(numOfResults);
+    }
+
+    function setRating() {
+        rating = $(this).attr('value');
+        console.log(rating);
+    }
+
+
+
+    // DISPLAYING BUTTONS
+
     if (personalButtons.length > 0) {
         personalButtons.forEach((item) => {
             newButton('personal', item);
@@ -69,16 +116,21 @@ $(document).ready(() => {
         newButton('sport', item);
     })
 
+
+
+    // CLICK FUNCTIONS
+
+    // Add gifs to the page when a query button is clicked.
     $(document.body).on('click', '.query-button', function () {
         queryTerm = $(this).text();
-        queryUrl = `http://api.giphy.com/v1/gifs/search?q=${queryTerm}&rating=${rating}&limit=${numOfResults}&api_key=${apiKey}`;
+        queryUrl = `http://api.giphy.com/v1/gifs/search?q=${queryTerm}&
+            rating=${rating}&limit=${numOfResults}&api_key=${apiKey}`;
         $('#gifs').empty();
         $.ajax({
             url: queryUrl,
             method: 'GET'
         }).then((response) => {
             response.data.forEach((item) => {
-
                 var newGif = $('<img>'),
                     newDiv = $('<div>');
                 newGif.attr({
@@ -89,51 +141,151 @@ $(document).ready(() => {
                     'data-animated': item.images.fixed_height.url
                 }).addClass('m-2 new-gif');
                 newDiv.addClass('float-left mt-2');
-                newDiv.append('<h6>Rating: ' + item.rating.toUpperCase() + '</h6>').append(newGif);
-
+                newDiv.append('<h6>Rating: ' + item.rating.toUpperCase() +
+                     '    <button id="save-button" type="button" class="btn btn-secondary btn-sm">Save</button></h6>');
+                newDiv.append(newGif);
 
                 $('#gifs').append(newDiv);
             })
         })
     })
 
+    // Show trending GIFs.
+    $('#trending').on('click', function () {
+        queryUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&rating=${rating}&limit=${numOfResults}`;
+        $('#gifs').empty();
+        $.ajax({
+            url: queryUrl,
+            method: 'GET'
+        }).then((response) => {
+            response.data.forEach((item) => {
+                var newGif = $('<img>'),
+                    newDiv = $('<div>');
+                newGif.attr({
+                    'src': item.images.fixed_height_still.url,
+                    'alt': item.title,
+                    'data-state': 'still',
+                    'data-still': item.images.fixed_height_still.url,
+                    'data-animated': item.images.fixed_height.url
+                }).addClass('m-2 new-gif');
+                newDiv.addClass('float-left mt-2');
+                newDiv.append('<h6>Rating: ' + item.rating.toUpperCase() +
+                     '    <button id="save-button" type="button" class="btn btn-secondary btn-sm">Save</button></h6>');
+                newDiv.append(newGif);
+
+                $('#gifs').append(newDiv);
+            })
+        })
+    })
+    
+    //Show random GIF.
+    $('#random').on('click', function () {
+        queryUrl = `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}&rating=${rating}`;
+        $('#gifs').empty();
+        $.ajax({
+            url: queryUrl,
+            method: 'GET'
+        }).then((response) => {
+            var newGif = $('<img>'),
+                newDiv = $('<div>');
+            newGif.attr({
+                'src': response.data.images.fixed_height_still.url,
+                'alt': response.data.title,
+                'data-state': 'still',
+                'data-still': response.data.images.fixed_height_still.url,
+                'data-animated': response.data.images.fixed_height.url
+            }).addClass('m-2 new-gif');
+            newDiv.addClass('float-left mt-2');
+                newDiv.append('<h6><button id="save-button" type="button" class="btn btn-secondary btn-sm">Save</button></h6>');
+                newDiv.append(newGif);
+            newDiv.append(newGif);
+
+            $('#gifs').append(newDiv);
+        })
+    })
+
+    // Play or pause the GIF.
     $('.container').on('click', '.new-gif', playAndPause);
 
+    // Adds a new button to the Personal Buttons group and adds it to local storage.
     $('#submit').on('click', function () {
-        alert($('#new-button').val());
         var newTerm = $('#new-button').val();
-        console.log('New term is: ' + newTerm);
         personalButtons.push(newTerm);
-        console.log(personalButtons);
-        localStorage.setItem(personalButtons, personalButtons);
+        localStorage.setItem('personalButtons', personalButtons);
         newButton('personal', newTerm);
+        $('#new-button').val('');
     })
 
+    // Displays or hides the login form.
     $('#login').on('click', function () {
         if ($('#login-collapse').attr('data-toggle') === 'hidden') {
-            $('#sign-up-collapse').hide();
-            $('#sign-up-collapse').attr('data-toggle', 'hidden');
-            $('#login-collapse').show();
-            loginDisplayAnime.play();
-            $('#login-collapse').attr('data-toggle', 'displayed');
+            showLogin();
         } else {
             $('#login-collapse').hide();
             $('#login-collapse').attr('data-toggle', 'hidden');
         }
     })
+
+    // Displays or hides the sign-up form.
     $('#sign-up').on('click', function () {
         if ($('#sign-up-collapse').attr('data-toggle') === 'hidden') {
-            $('#login-collapse').hide();
-            $('#login-collapse').attr('data-toggle', 'hidden');
-            $('#sign-up-collapse').show();
-            signUpDisplayAnime.play();
-            $('#sign-up-collapse').attr('data-toggle', 'displayed');
+            showSignUp();
         } else {
             $('#sign-up-collapse').hide();
             $('#sign-up-collapse').attr('data-toggle', 'hidden');
         }
     })
+
+    // Clear personal buttons.
+    $('#clear-local').on('click', function () {
+        localStorage.clear();
+        $('#personal-buttons').empty();
+    });
+
+    // Set the number of return results based on the clicked dropdown item.
+    $('.limit-dropdown').on('click', setNumOfResults);
+
+    // Set the rating of results based on the clicked dropdown item.
+    $('.rating-dropdown').on('click', setRating);
+
+    // Save a gif to the user's local storage.
+    $(document.body).on('click', '#save-button', function() {
+        var savedImage = $(this).parent().siblings(),
+        dataObj = {};
+        dataObj['src'] = savedImage.attr('src');
+        dataObj['alt'] = savedImage.attr('alt');
+        dataObj['data-still'] = savedImage.attr('data-still');
+        dataObj['data-animated'] = savedImage.attr('data-animated');
+
+        savedGifs.push(dataObj);
+        console.log(savedGifs);
+        localStorage.setItem('savedGifs', JSON.stringify(savedGifs));
+
+    })
+
+    // Show saved GIFs.
+    $('#saved-gifs').on('click', function() {
+        $('#gifs').empty();
+        savedGifs.forEach((item) => {
+            var newGif = $('<img>'),
+                newDiv = $('<div>');
+            newGif.attr({
+                'src': item.src,
+                'alt': item.alt,
+                'data-state': item['data-state'],
+                'data-still': item['data-still'],
+                'data-animated': item['data-animated']
+            }).addClass('m-2 new-gif');
+            newDiv.addClass('float-left mt-2');
+            newDiv.append(newGif);
+            $('#gifs').append(newDiv);
+        })
+    })
 })
+
+
+
+// ANIMATIONS
 
 let loginDisplayAnime = anime({
     targets: '#login-collapse',
