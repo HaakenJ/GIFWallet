9c2854d18763d2ba498a4b543d0d322eabcb86ed
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-01 16:47:55 -0700

    Moved much of the code in the click functions into separate functions in an attempt to make the code more modular.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,29 @@
-/* TODO:
-    - Figure out how to select multiple buttons to combine search terms
-
-    - Add progress or spinners using anime.js for when gifs are loading.*/
-
-
 $(document).ready(() => {
 
 
 
     // VARIABLES
 
-    var animal = ['dog', 'cat', 'rabbit', 'hamster', 'badger', 'dolphin', 'whale', 'platypus', 'kangaroo', 'wolverine', 'pig', 'horse', 'cow', 'snake'],
-        emotion = ['happy', 'funny', 'sad', 'angry', 'offended', 'bored', 'awkward', 'confused', 'excited', 'frustrated', 'hungry', 'tired', 'disappointed', 'embarrassed', 'scared', 'shocked', 'surprised', 'nervous'],
-        sport = ['baseball', 'football', 'basketball', 'hockey', 'soccer', 'cricket', 'rugby', 'rock climbing', 'snowboarding', 'skiing', 'skateboarding', 'wakeboarding', 'e-sports'],
-        apiKey = 'ARmBnNtTextf5564MTzhpY9zyTYN6Pde',
-        numOfResults = 10,
+    var animal = ['dog', 'cat', 'rabbit', 'hamster', 'badger', 'dolphin',
+            'whale', 'platypus', 'kangaroo', 'wolverine', 'pig', 'horse',
+            'cow', 'snake'
+        ],
+        emotion = ['happy', 'funny', 'sad', 'angry', 'offended', 'bored',
+            'awkward', 'confused', 'excited', 'frustrated', 'hungry', 'tired',
+            'disappointed', 'embarrassed', 'scared', 'shocked', 'surprised',
+            'nervous'
+        ],
+        sport = ['baseball', 'football', 'basketball', 'hockey', 'soccer',
+            'cricket', 'rugby', 'rock climbing', 'snowboarding', 'skiing',
+            'skateboarding', 'wakeboarding', 'e-sports'
+        ],
+        limit = 10,
         rating = 'g',
-        queryTerm = 'dog',
-        queryUrl = `http://api.giphy.com/v1/gifs/search?q=${queryTerm}&rating=${rating}&limit=${numOfResults}&api_key=${apiKey}`,
+        queryTerm = '',
         personalButtons,
         savedGifs;
 
-
+    // Retrive variables from localStorage if they exist.
     if (localStorage.getItem('personalButtons') === null) {
         personalButtons = [];
     } else {
@@ -40,6 +42,22 @@ $(document).ready(() => {
 
     // FUNCTIONS
 
+    // Creates a url to pass into an ajax call.  Has optional parameters with defaults.
+    function createUrl(type, rating = 'g', limit = '10', term = '', key = 'ARmBnNtTextf5564MTzhpY9zyTYN6Pde') {
+        if (type === 'query') {
+            return `http://api.giphy.com/v1/gifs/search?api_key=${key}&q=${term}&
+            rating=${rating}&limit=${limit}`
+        } else if (type === 'trending') {
+            return `http://api.giphy.com/v1/gifs/trending?api_key=${key}&
+            rating=${rating}&limit=${limit}`
+        } else if (type === 'random') {
+            return `http://api.giphy.com/v1/gifs/random?api_key=${key}&
+            rating=${rating}`
+        } else {
+            throw "not a valid endpoint type";
+        }
+    }
+    // Plays or pauses a clicked gif.
     function playAndPause() {
         var currentState = $(this).attr('data-state');
         if (currentState === 'still') {
@@ -50,7 +68,7 @@ $(document).ready(() => {
             $(this).attr('data-state', 'still');
         }
     }
-
+    // Creates a new button element and adds it to the passed category.
     function newButton(category, term) {
         var newBtn = $('<button>'),
             newCat = ('#' + category + '-buttons');
@@ -58,7 +76,47 @@ $(document).ready(() => {
         newBtn.text(term);
         $(newCat).append(newBtn);
     }
-
+    /* Creates a new gif element with all the necessary attributes by passing
+        a JSON formatted image object. */
+    function createNewGif(imgObj) {
+        var newGif = $('<img>'),
+            newDiv = $('<div>');
+        newGif.attr({
+            'src': imgObj.images.fixed_height_still.url,
+            'alt': imgObj.title,
+            'data-state': 'still',
+            'data-still': imgObj.images.fixed_height_still.url,
+            'data-animated': imgObj.images.fixed_height.url
+        }).addClass('m-2 new-gif');
+        newDiv.addClass('float-left mt-2');
+        if (imgObj.rating) {
+            newDiv.append('<h6>Rating: ' + imgObj.rating.toUpperCase() +
+                ` <button id="save-button" type="button" 
+                class="btn btn-secondary btn-sm">Save</button></h6>`);
+        } else {
+            newDiv.append(`<h6><button id="save-button" type="button" 
+            class="btn btn-secondary btn-sm">Save</button></h6>`);
+        }
+        newDiv.append(newGif);
+        return newDiv;
+    }
+    /* Creates a gif element using the format that the gifs are saved to
+        local storage in. */
+    function createGifFromSaved(imgObj) {
+        var newGif = $('<img>'),
+            newDiv = $('<div>');
+        newGif.attr({
+            'src': imgObj.src,
+            'alt': imgObj.alt,
+            'data-state': imgObj['data-state'],
+            'data-still': imgObj['data-still'],
+            'data-animated': imgObj['data-animated']
+        }).addClass('m-2 new-gif');
+        newDiv.addClass('float-left mt-2');
+        newDiv.append(newGif);
+        return newDiv;
+    }
+    // Shows the sign up form using anime.js.
     function showSignUp() {
         $('#login-collapse').hide();
         $('#login-collapse').attr('data-toggle', 'hidden');
@@ -66,7 +124,7 @@ $(document).ready(() => {
         signUpDisplayAnime.play();
         $('#sign-up-collapse').attr('data-toggle', 'displayed');
     }
-
+    // Shows the login form using anime.js.
     function showLogin() {
         $('#sign-up-collapse').hide();
         $('#sign-up-collapse').attr('data-toggle', 'hidden');
@@ -74,16 +132,35 @@ $(document).ready(() => {
         loginDisplayAnime.play();
         $('#login-collapse').attr('data-toggle', 'displayed');
     }
-
-    function setNumOfResults() {
-        numOfResults = $(this).attr('value');
-        console.log(numOfResults);
+    // Sets the variable limit based on a dropdown selection.
+    function setlimit() {
+        limit = $(this).attr('value');
     }
-
+    // Sets the variable rating based on a dropdown selection.
     function setRating() {
         rating = $(this).attr('value');
-        console.log(rating);
     }
+    /* Creates a new button from a passed in selector and adds it to the
+        personal buttons on the page and in local storage. */
+    function addToPersonalBtn(selector) {
+        var newTerm = $(selector).val();
+        personalButtons.push(newTerm);
+        localStorage.setItem('personalButtons', personalButtons);
+        newButton('personal', newTerm);
+        $(selector).val('');
+    }
+    /* Takes in one of the gif elements on the page and converts its
+        attributes into a JSON format to be stored in local storage. */
+    function returnGifJson(gif) {
+        var dataObj = {};
+        dataObj['src'] = gif.attr('src');
+        dataObj['alt'] = gif.attr('alt');
+        dataObj['data-still'] = gif.attr('data-still');
+        dataObj['data-animated'] = gif.attr('data-animated');
+        return dataObj;
+    }
+
+
 
 
 
@@ -112,112 +189,59 @@ $(document).ready(() => {
     // Add gifs to the page when a query button is clicked.
     $(document.body).on('click', '.query-button', function () {
         queryTerm = $(this).text();
-        queryUrl = `http://api.giphy.com/v1/gifs/search?q=${queryTerm}&
-            rating=${rating}&limit=${numOfResults}&api_key=${apiKey}`;
         $('#gifs').empty();
         $.ajax({
-            url: queryUrl,
+            url: createUrl('query', rating, limit, queryTerm),
             method: 'GET'
         }).then((response) => {
             response.data.forEach((item) => {
-                var newGif = $('<img>'),
-                    newDiv = $('<div>');
-                newGif.attr({
-                    'src': item.images.fixed_height_still.url,
-                    'alt': item.title,
-                    'data-state': 'still',
-                    'data-still': item.images.fixed_height_still.url,
-                    'data-animated': item.images.fixed_height.url
-                }).addClass('m-2 new-gif');
-                newDiv.addClass('float-left mt-2');
-                newDiv.append('<h6>Rating: ' + item.rating.toUpperCase() +
-                    '    <button id="save-button" type="button" class="btn btn-secondary btn-sm">Save</button></h6>');
-                newDiv.append(newGif);
-
-                $('#gifs').append(newDiv);
+                $('#gifs').append(createNewGif(item));
             })
         })
     })
 
     // Show trending GIFs.
     $('#trending').on('click', function () {
-        queryUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&rating=${rating}&limit=${numOfResults}`;
         $('#gifs').empty();
         $.ajax({
-            url: queryUrl,
+            url: createUrl('trending', rating, limit),
             method: 'GET'
         }).then((response) => {
             response.data.forEach((item) => {
-                var newGif = $('<img>'),
-                    newDiv = $('<div>');
-                newGif.attr({
-                    'src': item.images.fixed_height_still.url,
-                    'alt': item.title,
-                    'data-state': 'still',
-                    'data-still': item.images.fixed_height_still.url,
-                    'data-animated': item.images.fixed_height.url
-                }).addClass('m-2 new-gif');
-                newDiv.addClass('float-left mt-2');
-                newDiv.append('<h6>Rating: ' + item.rating.toUpperCase() +
-                    '    <button id="save-button" type="button" class="btn btn-secondary btn-sm">Save</button></h6>');
-                newDiv.append(newGif);
-
-                $('#gifs').append(newDiv);
+                $('#gifs').append(createNewGif(item));
             })
         })
     })
 
+
+
     //Show random GIF.
     $('#random').on('click', function () {
-        queryUrl = `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}&rating=${rating}`;
         $('#gifs').empty();
         $.ajax({
-            url: queryUrl,
+            url: createUrl('random'),
             method: 'GET'
         }).then((response) => {
-            var newGif = $('<img>'),
-                newDiv = $('<div>');
-            newGif.attr({
-                'src': response.data.images.fixed_height_still.url,
-                'alt': response.data.title,
-                'data-state': 'still',
-                'data-still': response.data.images.fixed_height_still.url,
-                'data-animated': response.data.images.fixed_height.url
-            }).addClass('m-2 new-gif');
-            newDiv.addClass('float-left mt-2');
-            newDiv.append('<h6><button id="save-button" type="button" class="btn btn-secondary btn-sm">Save</button></h6>');
-            newDiv.append(newGif);
-            newDiv.append(newGif);
-
-            $('#gifs').append(newDiv);
+            $('#gifs').append(createNewGif(response.data));
         })
     })
 
     // Play or pause the GIF.
     $('.container').on('click', '.new-gif', playAndPause);
 
-    // Adds a new button to the Personal Buttons group and adds it to local storage.
-    $('#submit').on('click', function () {
-        var newTerm = $('#new-button').val();
-        personalButtons.push(newTerm);
-        localStorage.setItem('personalButtons', personalButtons);
-        newButton('personal', newTerm);
-        $('#new-button').val('');
-    })
+    // Adds a new button to the Personal Buttons group.
+    $('#submit').on('click', () => {
+        addToPersonalBtn('#new-button')
+    });
 
     // Same function as above but for the mobile nav.
-    $('#submit-mobile').on('click', function () {
-        var newTerm = $('#new-button-mobile').val();
-        personalButtons.push(newTerm);
-        localStorage.setItem('personalButtons', personalButtons);
-        newButton('personal', newTerm);
-        $('#new-button-mobile').val('');
-    })
+    $('#submit-mobile').on('click', () => {
+        addToPersonalBtn('#new-button-mobile')
+    });
 
     // Displays or hides the login form.
     $('#login').on('click', function () {
         if ($('#login-collapse').attr('data-toggle') === 'hidden') {
-            console.log('clicked');
             showLogin();
         } else {
             $('#login-collapse').hide();
@@ -228,7 +252,6 @@ $(document).ready(() => {
     // Displays or hides the sign-up form.
     $('#sign-up').on('click', function () {
         if ($('#sign-up-collapse').attr('data-toggle') === 'hidden') {
-            console.log('clicked');
             showSignUp();
         } else {
             $('#sign-up-collapse').hide();
@@ -237,7 +260,7 @@ $(document).ready(() => {
     })
 
     // Clear personal info; i.e. buttons and saved GIFs.
-    $('#clear-local').on('click', function () {
+    $(document.body).on('click', '#clear-local', function () {
         localStorage.clear();
         $('#personal-buttons').empty();
         savedGifs = [];
@@ -245,24 +268,16 @@ $(document).ready(() => {
     });
 
     // Set the number of return results based on the clicked dropdown item.
-    $('.limit-dropdown').on('click', setNumOfResults);
+    $('.limit-dropdown').on('click', setlimit);
 
     // Set the rating of results based on the clicked dropdown item.
     $('.rating-dropdown').on('click', setRating);
 
     // Save a gif to the user's local storage.
     $(document.body).on('click', '#save-button', function () {
-        var savedImage = $(this).parent().siblings(),
-            dataObj = {};
-        dataObj['src'] = savedImage.attr('src');
-        dataObj['alt'] = savedImage.attr('alt');
-        dataObj['data-still'] = savedImage.attr('data-still');
-        dataObj['data-animated'] = savedImage.attr('data-animated');
-
-        savedGifs.push(dataObj);
-        console.log(savedGifs);
+        var savedImage = $(this).parent().siblings();
+        savedGifs.push(returnGifJson(savedImage));
         localStorage.setItem('savedGifs', JSON.stringify(savedGifs));
-
     })
 
     // Show saved GIFs.
@@ -270,18 +285,7 @@ $(document).ready(() => {
         $('#gifs').empty();
         if (savedGifs.length > 0) {
             savedGifs.forEach((item) => {
-                var newGif = $('<img>'),
-                    newDiv = $('<div>');
-                newGif.attr({
-                    'src': item.src,
-                    'alt': item.alt,
-                    'data-state': item['data-state'],
-                    'data-still': item['data-still'],
-                    'data-animated': item['data-animated']
-                }).addClass('m-2 new-gif');
-                newDiv.addClass('float-left mt-2');
-                newDiv.append(newGif);
-                $('#gifs').append(newDiv);
+                $('#gifs').append(createGifFromSaved(item));
             })
         }
     })
